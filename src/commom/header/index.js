0c9252672288b6d5681d6cae6e63a3e5ebb693fa
Yuhang Chen <hangyuchen@msn.com>
2019-05-05 23:46:32 +0800

    test

--- src/commom/header/index.js
+++ src/commom/header/index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
 import {
     HeaderWrapper,
     Logo,
@@ -8,74 +9,72 @@ import {
     SearhBar,
     SearchWrapper
 } from './style.js'
-import  { GlobalStyled } from '../../static/inconfont/iconfont'
+import { GlobalStyled } from '../../static/inconfont/iconfont'
 import { CSSTransition } from 'react-transition-group'
 
-class Header extends Component {
-    constructor(props) {
-        super(props)
-        this.state={
-            focused:false,
-        }
-        this.handlefocus= this.handlefocus.bind(this)
-        this.handleblur =this.handleblur.bind(this)
-    }
-
-    handlefocus(){
-        this.setState({
-            focused:true,
-        })
-    }
-
-    handleblur(){
-        this.setState({
-            focused:false,
-        })
-    }
-
-
-    render() {
-        return (
-            
-            <HeaderWrapper>  
-                <GlobalStyled/>
-            <Logo/>
-            <InnerContainer>
-                <NavList>
+const Header = (props) => {
+    return(
+    <HeaderWrapper>
+        <GlobalStyled />
+        <Logo />
+        <InnerContainer>
+            <NavList>
                 <NavItem className="left">首页</NavItem>
                 <NavItem className="left">下载APP</NavItem>
                 <NavItem className="left searchTop">
-                <SearchWrapper>
-                <CSSTransition
-                in={this.state.focused}
-                timeout={200}
-                classNames='slide'
-                >
-                <SearhBar
-                className ={this.state.focused ? 'focused':''}
-                onFocus={this.handlefocus}
-                onBlur={this.handleblur}
-                />
-                </CSSTransition>
-                <i className ={this.state.focused ? 'focused iconfont':'iconfont'}>&#xe637;
-                </i>
-                </SearchWrapper>
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={props.focused}
+                            timeout={200}
+                            classNames='slide'
+                        >
+                            <SearhBar
+                                className={props.focused ? 'focused' : ''}
+                                onFocus={props.handlefocus}
+                                onBlur={props.handleblur}
+                            />
+                        </CSSTransition>
+                        <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe637;
+                    </i>
+                    </SearchWrapper>
                 </NavItem>
                 <NavItem className="right writing">
-                <i className ="iconfont">&#xe616;</i>
-                写文章
-                </NavItem>
+                    <i className="iconfont">&#xe616;</i>
+                    写文章
+    </NavItem>
                 <NavItem className="right register">注册</NavItem>
                 <NavItem className="right loginGrey">登录</NavItem>
                 <NavItem className="right">Beta</NavItem>
                 <NavItem className="right">
-                <i className ="iconfont">&#xe600;</i>
+                    <i className="iconfont">&#xe600;</i>
                 </NavItem>
-                </NavList>
-            </InnerContainer>    
-            </HeaderWrapper>
-        )
+            </NavList>
+        </InnerContainer>
+    </HeaderWrapper>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        focused: state.focused,
+
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handlefocus() {
+            const action = {
+                type: 'search_focus'
+            }
+            dispatch(action)
+        },
+        handleblur() {
+            const action = {
+                type: 'search_blur'
+            }
+            dispatch(action)
+        }
     }
 }
 
-export default Header
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
--- src/commom/header/style.js
+++ src/commom/header/style.js
@@ -1,94 +1,6 @@
 import styled from 'styled-components'
 import logoPic from '../../static/logo.png'
 
-// export const HeaderWrapper = styled.div
-// `
-
-// height:56px;
-// border-bottom:1px soild #f0f0f0;
-// `
-
-// export const Logo = styled.a.attrs({
-//     href:'/'
-// })
-// `
-// top:0;
-// left:0;
-// display:inline-block;
-// width:100px;
-// height:56px;
-// background:url(${logoPic});
-// background-size:contain;
-// float:left;
-// `
-
-// export const Nav = styled.div
-// `
-// width:960px;
-// height:100%;
-// margin: 0 auto;
-// padding-right:70px;
-// box-sizing:border-box;
-// `
-// export const NavItem = styled.div
-// `
-// line-height:56px;
-// padding:0 15px;
-// font-size:17px;
-// color:#333;
-
-// &.left{
-//     float:left
-// }
-
-// &.right{
-//     float:right;
-//     color:#969696;
-// }
-// &.active{
-//     color:#ea6f5a;
-// }
-// `
-
-// export const NavSearch = styled.input.attrs({
-//     placeholder:'搜索'
-// })
-// `
-// box-sizing:border-box;
-// width:160px;
-// height:38px;
-// margin:9px;
-// border:none;
-// outline:none;
-// border-radius:19px;
-// background:#eee;
-// padding:0 20px;
-// font-size:14px;
-// margin-left:20px;
-// &::placeholder{
-//     color:#999;
-// }
-// `
-
-// export const Addition = styled.div
-// `
-// position:absolute;
-// right:0;
-// top:0;
-// height:56px;
-// `
-// export const Button = styled.div
-// `
-// margin-top:9px;
-// float:right;
-// line-height:38px;
-// border:1px soild #ec6149;
-// border-radius:19px;
-// margin-right:20px;
-// padding:0 20px;
-// `
-
-
 export const HeaderWrapper = styled.div
    `
     height:58px;
@@ -112,7 +24,7 @@ background-size:contain;
 float:left;
 `
 export const InnerContainer = styled.div
-`
+   `
 width: 960px;
 min-width:960px;
 height:58px;
@@ -120,7 +32,7 @@ margin:0 auto;
 background-color: #fff;
 `
 export const NavList = styled.ul
-`
+   `
 box-sizing: border-box;
 padding: 19px 10px;
 height: 58px;
@@ -129,21 +41,6 @@ height: 58px;
 export const SearchWrapper = styled.div
 `
 position: relative;
-.slide-enter{
-   width:160px;
-   transition:all .2s ease-out;
- }
- .slide-enter-active{
-   width:200px;
-
-    
-}
-.slide-exit{
-   width:200px;
-}
-.slide-exit-active{
-   transition:all .2s ease-out;
-}
 .iconfont{
    position: absolute;
     right: 10px;
@@ -151,18 +48,18 @@ position: relative;
     width: 30px;
     line-height: 30px;
     border-radius: 30px;
-    background-color: red;
+   
     text-align: center;
     &.focused{
       color:#777;  
-      background-color: green;
+      
     }
 }
 `
 export const SearhBar = styled.input.attrs({
    placeholder: 'Search'
 })
-`
+   `
 box-sizing:border-box;
 width:160px;
 border:none;
@@ -178,12 +75,26 @@ color:#666;
 }
 &.focused{
    color:#777;  
-   width:250px;  
+   width:200px;  
  }
+ &.slide-enter{
+   width:160px;
+   transition:all .2s ease-out;
+ }
+ &.slide-enter-active{
+   width:200px;
+}
+&.slide-exit{
+    width:200px;
+}
+&.slide-exit-active{
+   transition:all .2s ease-out;
+   width:160px;
+}
 
 `
 export const NavItem = styled.li
-`
+   `
 padding: 0 10px;
 &.left{
   float:left
